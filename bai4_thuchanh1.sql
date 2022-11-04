4a392cab035b734f5ebc1a999de3cbfcad7f7d0e
TuanDao2602 <110024900+TuanDao2602@users.noreply.github.com>
2022-11-04 09:42:32 +0700

    Add files via upload

---
+++ bai4_thuchanh1.sql
@@ -0,0 +1,14 @@
+use quanlysinhvien;
+select address , count(studentid) as ' số lượng học viên'
+from student
+group by Address;
+
+SELECT S.StudentId, S.StudentName, AVG(Mark)
+FROM Student S join Mark M on S.StudentId = M.StudentId
+GROUP BY S.StudentId, S.StudentName
+HAVING AVG(Mark) > 15;
+
+SELECT S.StudentId, S.StudentName, AVG(Mark)
+FROM Student S join Mark M on S.StudentId = M.StudentId
+GROUP BY S.StudentId, S.StudentName
+HAVING AVG(Mark) >= ALL (SELECT AVG(Mark) FROM Mark GROUP BY Mark.StudentId);
